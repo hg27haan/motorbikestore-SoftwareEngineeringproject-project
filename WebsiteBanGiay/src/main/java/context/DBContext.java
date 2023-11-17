9def93cee55dd93e761278bab32f8a9ac0a1b41e
Han Huynh Gia <94831616+hg27haan@users.noreply.github.com>
2023-11-18 00:33:21 +0700

    CLONE PROJECT THÀNH CÔNG

--- WebsiteBanGiay/src/main/java/context/DBContext.java
+++ WebsiteBanGiay/src/main/java/context/DBContext.java
@@ -20,11 +20,11 @@ public class DBContext {
     }   
     /*Insert your other code right after this comment*/
     /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
-    private final String serverName = "localhost";
+    private final String serverName = "GIAHANHUYNH";
     private final String dbName = "QuanLyBanGiay";
     private final String portNumber = "1433";
     private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
-    private final String userID = "demo4";
-    private final String password = "12345678";
+    private final String userID = "sa";
+    private final String password = "123456";
 }
  
